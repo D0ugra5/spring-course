a6e0cab9fda2984a2e2b92e95364a79a0171163e
Douglas Sousa <douglasmb87@gmail.com>
2023-06-27 10:31:40 -0300

    feat: add ignore mapper

--- src/main/java/med/voll/api/mapper/MedicoMapper.java
+++ src/main/java/med/voll/api/mapper/MedicoMapper.java
@@ -3,11 +3,14 @@ package med.voll.api.mapper;
 import med.voll.api.domain.medico.DadosCadastroMedico;
 import med.voll.api.domain.medico.Medico;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public abstract class MedicoMapper {
     public static final  MedicoMapper INSTANCE = Mappers.getMapper(MedicoMapper.class);
+
+    @Mapping(target = "id", ignore = true)
 
     public abstract Medico toMedico( DadosCadastroMedico dadosCadastroMedico);
 
--- src/main/java/med/voll/api/mapper/PacienteMapper.java
+++ src/main/java/med/voll/api/mapper/PacienteMapper.java
@@ -3,11 +3,15 @@ package med.voll.api.mapper;
 import med.voll.api.domain.paciente.DadosCadastroPaciente;
 import med.voll.api.domain.paciente.Paciente;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public abstract class PacienteMapper {
     public static final PacienteMapper INSTANCE = Mappers.getMapper(PacienteMapper.class);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "ativo", ignore = true)
     public abstract Paciente toPaciente(DadosCadastroPaciente dadosCadastroPaciente);
 
 }
